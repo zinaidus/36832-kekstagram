032c5d3ef4bd6a03b751d88cf1f4a581e6b1893b
Zinaida Semenova <thelandofspirits@gmail.com>
2016-08-18 20:12:32 -0500

    сделаны стилистические изменения и усовершенствован код, учитывая правки

--- build/js/check.js
+++ build/js/check.js
@@ -1,45 +1,35 @@
-
+'use strict';
 function getMessage(a, b) {
-	
 	var message;
 	if (typeof a == 'boolean') {
-
-		if(a == true){
+        if(a == true){
 			message = 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
 		} else {
 			message = 'Переданное GIF-изображение не анимировано';
 		}
-
 	}  
 
 	if (typeof a == 'number') {
-
 		message = 'Переданное SVG-изображение содержит ' + a + 'объектов и ' + b * 4 + ' атрибутов';
 	}
 
+	if (Array.isArray(a)) {
+		if (Array.isArray(b)){
+			var n = 0;
+			if (a.length == b.length) n = a.length;
+			else if (a.length < b.length) n = a.length;
+			else n = b.length;
 
-
-	if (a.constructor == 'Array') {
-		
-
-		var i;
-
-		if(b.constructor == 'Array'){
-					
-			var n = a.length == b.length ? a.length : a.length < b.length ? a.length : b.length;
 			var artifactsSquare = 0;
-			for (i = 0; i<n; i++){
+			for (var i = 0; i<n; i++){
       			artifactsSquare += a[i] * b[i];      
 			}
 			message = 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
 		}
 	} else{
-		var amountOfRedPoints = 0;
-		for (i = 0; i < a.length; i++){
-			amountOfRedPoints += a[i]; 
-		}
+		var amountOfRedPoints = a.reduce( (prev, cur) => (prev+cur), 0 )
+		
 		message = 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
-
 	}
 	return message;
 }
