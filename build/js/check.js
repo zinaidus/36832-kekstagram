251d1cb6df749af2a0441170e3d16488a7139dbd
Zinaida Semenova <thelandofspirits@gmail.com>
2016-08-19 21:36:37 -0500

    улучшена функция getMessage, недочеты  исправлены

--- build/js/check.js
+++ build/js/check.js
@@ -1,25 +1,19 @@
 'use strict';
-function getMessage(a, b) {
+function getMessage(a, b){
 	var message;
-	if (typeof a == 'boolean') {
-        if(a == true){
+	if (typeof a === 'boolean'){
+        if(a === true){
 			message = 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
 		} else {
 			message = 'Переданное GIF-изображение не анимировано';
 		}
 	}  
-
-	if (typeof a == 'number') {
+	if (typeof a === 'number'){
 		message = 'Переданное SVG-изображение содержит ' + a + 'объектов и ' + b * 4 + ' атрибутов';
 	}
-
-	if (Array.isArray(a)) {
+	if (Array.isArray(a)){
 		if (Array.isArray(b)){
-			var n = 0;
-			if (a.length == b.length) n = a.length;
-			else if (a.length < b.length) n = a.length;
-			else n = b.length;
-
+			var n = Math.min(a.length, b.length);
 			var artifactsSquare = 0;
 			for (var i = 0; i<n; i++){
       			artifactsSquare += a[i] * b[i];      
@@ -27,7 +21,9 @@ function getMessage(a, b) {
 			message = 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
 		}
 	} else{
-		var amountOfRedPoints = a.reduce( (prev, cur) => (prev+cur), 0 )
+		var amountOfRedPoints = a.reduce(function(a, b){
+        return a + b;
+           }, 0);
 		
 		message = 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
 	}
