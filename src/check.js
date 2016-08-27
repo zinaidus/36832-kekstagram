059efd14de520d69c1eff381a190acefdf05b3c0
Zinaida Semenova <thelandofspirits@gmail.com>
2016-08-26 22:54:54 -0500

    Заменены фвйлы в js

--- src/check.js
+++ src/check.js
@@ -1,47 +1,38 @@
+'use strict';
 
 function getMessage(a, b) {
-	
-	var message;
-	if (typeof a == 'boolean') {
+    var message;
 
-		if (a == true){
-			message = 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
-		} else {
-			message = 'Переданное GIF-изображение не анимировано';
-		}
+    if (typeof a === 'boolean') {
+        if (a === true) {
+            message = 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+        } else {
+            message = 'Переданное GIF-изображение не анимировано';
+        }
+    }
 
-	}  
+    if (typeof a === 'number') {
+        message = 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
+    }
 
-	if (typeof a == 'number') {
-		message = 'Переданное SVG-изображение содержит ' + a + 'объектов и ' + b * 4 + ' атрибутов';
-	}
+    if (Array.isArray(a)) {
+        if (Array.isArray(b)) {
+            var n = Math.min(a.length, b.length);
+            var artifactsSquare = 0;
 
+            for (var i = 0; i < n; i++) {
+                artifactsSquare += a[i] * b[i];
+            }
 
+            message = 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+        }
+    } else {
+        var amountOfRedPoints = a.reduce(function(a, b) {
+            return a + b;
+        }, 0);
 
-	if (a.constructor == 'Array') {
-		
+        message = 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
+    }
 
-		var i;
-
-		if(b.constructor == 'Array'){
-					
-			var n = a.length == b.length ? a.length : a.length < b.length ? a.length : b.length;
-			var artifactsSquare = 0;
-			for (i = 0; i<n; i++){
-      			artifactsSquare += a[i] * b[i];      
-			}
-			message = 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
-		}
-	} else{
-		var amountOfRedPoints = 0;
-		for (i = 0; i < a.length; i++){
-			amountOfRedPoints += a[i]; 
-		}
-		message = 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
-
-	}
-
-	return message;
+    return message;
 }
-
-
